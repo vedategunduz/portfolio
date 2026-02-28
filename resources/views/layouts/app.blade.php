9c413444aa744ebd77dd7499543f9484c6d6d78a
Vedat Emre GÜNDÜZ <vedat.bilisim@outlook.com>
2026-02-28 13:47:17 +0300

    Add new font files for Instrument Sans Latin Extended weights 500 and 600
    - Added instrument-sans-latin-ext-500-normal.woff2
    - Added instrument-sans-latin-ext-600-normal.woff2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,6 @@
         <x-seo />
     @endif
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600&display=swap" rel="stylesheet" />
-
     <script>
         (() => {
             const stored = localStorage.getItem('theme');
